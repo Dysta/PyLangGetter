8d41693f8d3c7b20f074c804f0481bc1b7f98922
Dysta <xav4098@gmail.com>
2021-03-03 22:51:01 +0100

    minor refactor

--- PyLangGetter.py
+++ PyLangGetter.py
@@ -1,41 +1,12 @@
-import urllib.request as urireq
 import os
 import sys
+from utils import Utils
 
-def write_content_to_file(filename, content, isByte):
-    mode = "wb" if isByte else "w"
-    with open(filename, mode) as file:
-        file.write(content)
-
-def get_content_from_uri(uri, decode):
-    with urireq.urlopen(uri) as file:
-        if decode:
-            return file.read().decode("utf-8")
-        return file.read()
-
-def parse_version(version_file):
-    with open(version_file, "r") as file:
-        content = file.read()
-    file_list = content.replace("&f=", "").replace(",", "_").split("|")[:-1]
-    return [f"{name}.swf" for name in file_list]
-
-def parse_args(args):
-    args = args[1:]
-    default_lang = ["fr", "de", "en", "it", "es", "pt", "nl"]
-    if len(args) < 1:
-        return default_lang
-        
-    for arg in args:
-        if arg not in default_lang:
-            print(f"Unknow lang {arg}, available lang : {default_lang}")
-            exit(0)
-    return args
-
-BASE_URL = "http://dofusretro.cdn.ankama.com/lang/"
-BASE_DIR = "lang/"
+BASE_URL: str = "http://dofusretro.cdn.ankama.com/lang/"
+BASE_DIR: str = "lang/"
 
 if __name__ == "__main__":
-    lang_list = parse_args(sys.argv)
+    lang_list = Utils.parse_args(sys.argv)
     print(f"Getting lang from this : {lang_list}")
 
     if not os.path.exists(BASE_DIR + "swf/"):
@@ -44,8 +15,8 @@ if __name__ == "__main__":
     versions_uri = BASE_URL + "versions.swf"
     versions_dir = BASE_DIR + "versions.swf"
 
-    content = get_content_from_uri(versions_uri, False)
-    write_content_to_file(versions_dir, content, True)
+    content = Utils.get_content_from_uri(versions_uri, False)
+    Utils.write_content_to_file(versions_dir, content, True)
     
     countLang = 1
     countFile = 1
@@ -55,18 +26,18 @@ if __name__ == "__main__":
         url_version  = BASE_URL + version_name
         dir_version  = BASE_DIR + version_name
 
-        content = get_content_from_uri(url_version, True)
-        write_content_to_file(dir_version, content, False)
+        content = Utils.get_content_from_uri(url_version, True)
+        Utils.write_content_to_file(dir_version, content, False)
         
-        file_list = parse_version(dir_version)
+        file_list = Utils.parse_version(dir_version)
 
         for file in file_list:
             url_file = BASE_URL + "swf/" + file
             dir_file = BASE_DIR + "swf/" + file
 
             print(f"[{countLang}/{len(lang_list)}][{countFile}/{len(file_list)}] \t Get lang {file}")
-            content = get_content_from_uri(url_file, False)
-            write_content_to_file(dir_file, content, True)
+            content = Utils.get_content_from_uri(url_file, False)
+            Utils.write_content_to_file(dir_file, content, True)
             countFile+=1
         
         countFile = 1
---
+++ utils.py
@@ -0,0 +1,71 @@
+import urllib.request as urireq
+
+
+class Utils:
+    DEFAULT_LANG: list = ["fr", "de", "en", "it", "es", "pt", "nl"]
+
+    @staticmethod
+    def write_content_to_file(filename: str, content: str, isByte: bool) -> None:
+        """Write content to the file filename
+
+        Args:
+            filename (str): the filename destination
+            content (str): the content to write
+            isByte (bool): true if the content is raw bytes, false otherwise
+        """
+        mode = "wb" if isByte else "w"
+        with open(filename, mode) as file:
+            file.write(content)
+
+    @staticmethod
+    def get_content_from_uri(uri: str, decode: bool) -> str:
+        """Get the content of a file from the uri
+
+        Args:
+            uri (str): the url of the file
+            decode (bool): true if decode the content in utf-8, false otherwise
+
+        Returns:
+            str: the content of the file
+        """
+        with urireq.urlopen(uri) as file:
+            if decode:
+                return file.read().decode("utf-8")
+            return file.read()
+
+    @staticmethod
+    def parse_version(version_file: str) -> list:
+        """Parse the given version file and return a list of the swf files
+
+        Args:
+            version_file (str): the version_*.txt to parse
+
+        Returns:
+            list: a list of swf file name 
+        """
+        with open(version_file, "r") as file:
+            content = file.read()
+        file_list = content.replace("&f=", "").replace(",", "_").split("|")[:-1]
+        return [f"{name}.swf" for name in file_list]
+
+    @staticmethod
+    def parse_args(args: list) -> list:
+        """Parse the CLI args to determine the swg lang to get
+
+        Args:
+            args (list): the list of lang to fetch
+
+        Raises:
+            ValueError: When a lang arg is not available
+        
+        Returns:
+            list: the list of the language lang to get
+        """
+        args = args[1:]
+        if len(args) < 1:
+            return Utils.DEFAULT_LANG
+            
+        for arg in args:
+            if arg not in Utils.DEFAULT_LANG:
+                raise ValueError(f"Unknow lang {arg}, available lang : {Utils.DEFAULT_LANG}")
+        return args
